0a60d60e2f7660e1a075f9edae518fe6e8afa919
AlexSinMas23 <ba.mota@iesdoctorbalmis.com>
2021-10-14 20:10:53 +0200

    añado lógica para mostrar los números que son pulsados

--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -20,9 +20,8 @@ namespace Calculadora
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
+        private void Button_Click(object sender, RoutedEventArgs e)
+            => VistaTexto.Text += (sender as Button).Tag;
     }
 }
